91dbcd2b4965e06d9ed85e3270118e0fdad4819a
leandro <romeroleandro0711@gmail.com>
2023-05-23 20:40:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
+    // iterate from the beginning to the middle
+    for (let i = 0; i < word.length / 2; i++) {
+      // check each letter to the correponding letter from the end
+      const j = word.length - 1 - i;
+      // if any letters don't match, return false
+      if (word[i] !== word[j]) return false;
+    }
+  
+    // return true
+    return true;
+  }
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
